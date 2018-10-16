d3c7051cd377ce5bb226078c77112072b8272143
Jacob Menashi <jbmenashi@gmail.com>
2018-10-16 18:12:36 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,7 @@ def apply_coupons(cart, coupons)
           cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
           cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
         end
-          
+      
         end
       end
     end
