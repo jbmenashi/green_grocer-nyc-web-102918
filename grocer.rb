31b4792fd499d7008d1620b185bb8af477b19883
Jacob Menashi <jbmenashi@gmail.com>
2018-10-16 18:57:22 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -34,9 +34,8 @@ end
 
 def apply_clearance(cart)
   cart.each do |name, info|
-      if cart[name][:clearance] == "true"
-        cart[name][:price] *= 0.8
-      end
+    if cart[name][:clearance] == "true"
+      cart[name][:price] *= 0.8
     end
   end
   cart
