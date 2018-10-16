98c0a56441e1a34a291daf4ea4f269e1a9e88832
Jacob Menashi <jbmenashi@gmail.com>
2018-10-16 18:15:59 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -20,9 +20,9 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
   name = coupon[:item]
     if cart[name] == coupon[:item] && cart[name][:count] >= coupon[:num]
-        if cart["#{name} W/COUPON"]
-          cart["#{name} W/COUPON"][:count] += 1
-        else
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
           cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
           cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
         end
